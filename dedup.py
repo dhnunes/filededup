7f88ab952df01422cccdd3b787741a8ef9d7699e
Diogo Nunes <diogo.hap@gmail.com>
2018-06-11 20:21:18 -0300

    Fix Issue #1

--- dedup.py
+++ dedup.py
@@ -1,6 +1,19 @@
 import hashlib
 import os
 
+
+def file_hasher(path):
+    BUF_SIZE = 65536
+    sha256 = hashlib.sha256()
+    with open(path, 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            sha256.update(data)
+        return(sha256.hexdigest())
+
+
 root_of_tests = "/home/dnunes/avocado/job-results/job-2018-06-11T14.35-4958f56"
 
 result = dict()
@@ -10,9 +23,8 @@ for root, dirs, files in os.walk(root_of_tests):
     for name in files:
         all_files.append(os.path.join(root, name))
 
-
 for file in all_files:
-    result[file] = hashlib.sha256(file.encode()).hexdigest()
+    result[file] = file_hasher(file)
 
 for key, value in result.items():
     print(key, value)
