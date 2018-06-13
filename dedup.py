68087167b937af40b0fa1fd1395f17a1c4dda4f9
Diogo Nunes <diogo.hap@gmail.com>
2018-06-13 17:53:43 -0300

    Organizing code as functions, annotating these functions and solving #2

--- dedup.py
+++ dedup.py
@@ -1,45 +1,52 @@
 import hashlib
 import os
 import sys
+import pdb
 
 
-def file_hasher(path):
-    BUF_SIZE = 65536
-    sha256 = hashlib.sha256()
-    with open(path, 'rb') as f:
+def file_walker(path: str) -> tuple:
+    all_files = list()
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if os.path.getsize(os.path.join(root, file)):
+                all_files.append(os.path.join(root, file))
+
+    return(file_hash_association(tuple(all_files)))
+
+
+def file_hasher(file: str) -> str:
+    buffer_size = 65536
+    sha256_object = hashlib.sha256()
+    with open(file, 'rb') as file:
         while True:
-            data = f.read(BUF_SIZE)
+            data = file.read(buffer_size)
             if not data:
                 break
-            sha256.update(data)
-        return(sha256.hexdigest())
+            sha256_object.update(data)
+    return(sha256_object.hexdigest())
 
 
-root_of_tests = sys.argv[1]
+def file_hash_association(hashed_files: tuple) -> dict:
+    results = dict()
 
-result = dict()
-all_files = list()
-all_dirs = list()
-for root, dirs, files in os.walk(root_of_tests):
-    for name in files:
-        all_files.append(os.path.join(root, name))
+    for file in hashed_files:
+        results[file] = file_hasher(file)
 
-for file in all_files:
-    result[file] = file_hasher(file)
+    return(results)
 
+
+print(file_walker(sys.argv[1]))
+
+# all_tests = list()
+# for value in result.values():
+#     all_tests.append(value)
+
+# for test in all_tests:
+#     print(all_tests.count(test))
+# if all_tests.count(test) > 1:
 # for key, value in result.items():
-#     print(key, value)
-all_tests = list()
-for value in result.values():
-    all_tests.append(value)
-
-for test in all_tests:
-    print(all_tests.count(test))
-
-for test in all_tests:
-    if all_tests.count(test) > 1:
-        for key, value in result.items():
-            if value == test:
-                print(key)
-                print(test)
-                print(value)
+#     if value == test:
+#         print(key)
+#         print(test)
+#         print(value)
