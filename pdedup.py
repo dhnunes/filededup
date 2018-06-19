d01d24510d4fb6dee62d8531f47b6e654af365d0
Diogo Nunes <diogo.hap@gmail.com>
2018-06-19 00:12:13 -0300

    Code Cleanup

--- pdedup.py
+++ pdedup.py
@@ -7,26 +7,8 @@ docstring
 from Library.filehasher import FileHasher
 from Library.cmdargs import CmdArgs
 
-#print(args)
-#print(os.path.isdir(args.dir_path))
+if __name__ == '__main__':
 
-
-args = CmdArgs.cmd_parsed_args()
-
-print(args)
-
-# file = FileHasher(args.dir_path)
-
-# print(file.file_walker())
-
-# all_tests = list()
-# for value in result.values():
-#     all_tests.append(value)
-# for test in all_tests:
-#     print(all_tests.count(test))
-# if all_tests.count(test) > 1:
-# for key, value in result.items():
-#     if value == test:
-#         print(key)
-#         print(test)
-#         print(value)
+    args = CmdArgs.cmd_parsed_args()
+    file = FileHasher(args["dir_path"])
+    result = file.file_walker()
